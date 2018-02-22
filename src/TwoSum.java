a56eec00b79dfcd9467e4807807da7a983882941
niannian liu <liunn@fenqi.im>
2018-02-22 14:20:51 +0800

    2018-2-22 14:20:45

---
+++ src/TwoSum.java
@@ -0,0 +1,90 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * <p>
+ * tag: Array
+ * difficulty: Easy
+ * 由结果来推算索引，典型的两个变量计算问题，用hash table速度最快
+ * <p>
+ * Created by liunn on 2018/2/22.
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 2, 4};
+        int target = 6;
+        function1(nums, target);
+    }
+
+    /**
+     * 自己的答案，暴力循环，时间复杂度较高
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] function1(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 通过 two-pass hash table 解决，时间复杂度较低
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] function2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 通过 one-pass hash table 解决，时间复杂度较低
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] function3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
